801165bd42d1a8e05ce23b3afa9d4bee9b4d01cf
Sophie <sophiejoliver19+1@gmail.com>
2021-09-28 02:17:45 +0000

    template for /urls/new which contains a form that submits a POST req for /urls

--- express_server.js
+++ express_server.js
@@ -2,6 +2,11 @@ const express = require('express');
 const app = express();
 const PORT = 8080;
 
+// middleware -- converts POST request body from a Buffer to a string (human-readable)
+// adds the data to the req object under the key body
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended: true}));
+
 // tells Express app to use EJS as its templating/view engine 
 app.set("view engine", "ejs");
 
@@ -20,9 +25,25 @@ app.get("/urls.json", (req, res) => {
 
 // points to template for table with short & long urls 
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase };
+  const templateVars = { urls: urlDatabase }; // { urls: { shortURL: longURL, shortURL: longURL }}
   res.render("urls_index", templateVars);
 })
+
+// route definition to get the form -- points to template to create new short url
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+})
+
+// defines route that will match the form POST request & handle it 
+app.post("/urls", (req, res) => {
+  console.log(req.body); // log the POST request body to the console
+  res.send("Ok"); // respond with "Ok" -- to be replaced
+})
+
+// generate a random shortURL (string)
+const generateRandomString = () => {
+
+}
 
 // points to template for rending info about a single url 
 app.get("/urls/:shortURL", (req, res) => {
